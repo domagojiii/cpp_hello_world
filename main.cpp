42acc4e5dd4444012cc1657ffa97ca82bfbc4999
domagojiii <92570830+domagojiii@users.noreply.github.com>
2021-10-15 11:50:43 +0200

    zadatak 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <cstdio>
+#include <iostream>
+using namespace std;
 
 int main() {
-  printf("Hello, world git!\n");
+  int a, b, c, d;
+  std::cout << "Upisi A: ";
+  std::cin >> a;
+  std::cout << "Upisi B: ";
+  std::cin >> b;
+  std::cout << "Upisi C: ";
+  std::cin >> c;
+  std::cout << "Upisi D: ";
+  std::cin >> d;
+  //std::cout << "A = " << a;
+  cout << oct << a/b << endl;
+  cout << dec << a/c << endl;
+  cout << hex << a/d << endl;
   return 0;
 }
