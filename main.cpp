33cedd2e8da27399626c0e5926d3a447afaa540f
domagojiii <92570830+domagojiii@users.noreply.github.com>
2021-10-19 19:04:58 +0200

    luka

--- main.cpp
+++ main.cpp
@@ -2,18 +2,11 @@
 using namespace std;
 
 int main() {
-  int a, b, c, d;
-  std::cout << "Upisi A: ";
-  std::cin >> a;
-  std::cout << "Upisi B: ";
-  std::cin >> b;
-  std::cout << "Upisi C: ";
-  std::cin >> c;
-  std::cout << "Upisi D: ";
-  std::cin >> d;
-  //std::cout << "A = " << a;
-  cout << oct << a/b << endl;
-  cout << dec << a/c << endl;
-  cout << hex << a/d << endl;
+  int decilitri = 1798282; 
+  int hektolitri, litre;
+  hektolitri = decilitri / 1000;
+  litre = decilitri / 10;
+  std::cout << decilitri << "\n";
+  std::cout << hektolitri << " " << litre  << " " << decilitri << "\n";
   return 0;
 }
