996e439b13463279acc52ebae7fc9466c51283f8
domagojiii <92570830+domagojiii@users.noreply.github.com>
2021-11-14 13:58:29 +0100

    zadatak 1

--- zad1.cpp
+++ zad1.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 int main() {
+  int n, x, max, max2;
+  cin >> n;
+  // prvi broj je automatski max
+  cin >> x;
+  max = x;
+  // drugi broj
+  cin >> x;
+  if(x>max){
+    max2 = max;
+    max = x;
+  }else{
+    max2 = x;
+  }
+  for(int i=2; i<n; i++){
+    cin >> x;
+    if(x>max){
+      max2 = max;
+      max = x;
+    }
+    if(x<max && x>max2){
+      max2 =x;
+    }
+  }
+  cout << "drugi najveci je " << max2;
   return 0;
 }
