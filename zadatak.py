26cdb951fbbe9a2df310c8de5d244ff6ed2eb0f6
domagojiii <92570830+domagojiii@users.noreply.github.com>
2021-11-30 18:15:18 +0100

    29.11. 1

--- zadatak.py
+++ zadatak.py
@@ -1,26 +1,8 @@
 import math
 
-def diskriminanta(a,b,c):
-	d = math.pow(b,2) - 4*a*c
-	if d > 0:
-		return d
-	else:
-		return 0
-
-def predznak(x):
-	return x*(-1)
-
-def umnozak_nultocki(a,b,c):
-	return ((predznak(b) + math.sqrt(diskriminanta(a,b,c)))/(2*a)) * ((predznak(b) - math.sqrt(diskriminanta(a,b,c)))/(2*a))
-
-def main():
-	a = int(input('unesi a:'))
-	b = int(input('unesi b:'))
-	c = int(input('unesi c:'))
-	while a != 0 and b != 0 and c != 0:
-		print(umnozak_nultocki(a,b,c))
-		a = int(input('unesi a:'))
-		b = int(input('unesi b:'))
-		c = int(input('unesi c:'))
-
-main()
+vrijeme_12_24 = int(input('Unesite prikaz vremena koje vas sat podrzava (24/12): '))
+vrijeme = int(input('Unesite vrijeme koje prikazuje vas sat sada: '))
+minute = int(input('Unesite trazene minute: '))
+vrijeme_sastanka_sat = (vrijeme + 80) % vrijeme_12_24
+vrijeme_sastanka_sat_minute = vrijeme_sastanka_sat + (minute / 60)
+print('Tocno vrijeme sastanka zamaskirano decimalnim zapisom je: {:.3f}'.format(vrijeme_sastanka_sat_minute))
