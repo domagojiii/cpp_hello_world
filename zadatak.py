07af0edcdb2c9e7f3d4dca53f189024ba69fc6e3
domagojiii <92570830+domagojiii@users.noreply.github.com>
2021-12-14 18:15:34 +0100

    zadatak 1 14.12.21

--- zadatak.py
+++ zadatak.py
@@ -1,25 +1,16 @@
-def ispis_datum(d, m, g):
-	print('Uneseni datum je {}.{}.{}'.format(d, m, g), end='')
-
-def ispis_dan(d, m, g):
-	x = d
-	for i in range(m):
-		if i == 1 or i == 3 or i == 5 or i == 7 or i == 8 or i == 10 or i == 12:
-			x += 31
-		if i == 2:
-			x += 28
-		if i == 4 or i == 6 or i == 9 or i == 11:
-			x += 30
-	print(' i to je {}. dan u godini.'.format(x))
-
-def main():
-	a = 1
-	while a != 0:
-		d = int(input('Unesite dan: '))
-		m = int(input('Unesite mjesec: '))
-		g = int(input('Unesite godinu: '))
-		ispis_datum(d, m, g)
-		ispis_dan(d, m, g)
-		a = int(input('Za nastavak unesi 1, za kraj unesi 0: '))
-
-main()
+recenica = input("Upisi string:")
+a = recenica.count('a')
+e = recenica.count('e')
+i = recenica.count('i')
+o = recenica.count('o')
+u = recenica.count('u')
+samoglasnici = a+e+i+o+u
+print('a {}, e {}, i {}, o {}, u {}'.format(a, e, i, o, u))
+razmaci = recenica.count(' ')
+suglasnici = len(recenica) - samoglasnici - razmaci - 1
+if suglasnici > samoglasnici:
+	print('Ukupna brojka samoglasnika je manja od suglasnika i ona iznosi {}'.format(samoglasnici))
+if suglasnici == samoglasnici:
+	print('Ukupna brojka samoglasnika je jednak kao suglasnika i ona iznosi {}'.format(samoglasnici))
+if suglasnici < samoglasnici:
+	print('Ukupna brojka samoglasnika je veca od suglasnika i ona iznosi {}'.format(samoglasnici))
