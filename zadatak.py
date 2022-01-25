55de6fd27c84e4f0d48a562eb15c083cdbcaaa3a
domagojiii <92570830+domagojiii@users.noreply.github.com>
2022-01-25 18:59:54 +0100

    drugi

--- zadatak.py
+++ zadatak.py
@@ -1,42 +1,54 @@
-from random import randrange
+baza = '/Users/domagoj/a/glumice.txt'
 
-rijecnik = {}
-izm = []
+def unos_zapisa():
+	k = int(input('Kljuc >> '))
+	ime = input('Unesi ime >> ')
+	prezime = input('Unesi prezime >> ')
+	doc = open(baza, 'r')
+	linije = doc.readlines()
+	for l in linije:
+		if l.startswith(str(k)):
+			print('Postoji zapis s istim kljucem')
+			print('Ponovo pozovi naredbu za upis u bazu podataka')
+			doc.close()
+			return
+	doc.close()
+	doc = open(baza, 'a')
+	doc.write(str(k)+'|'+ime+' '+prezime+'\n')
+	doc.close()
 
-def prijevod(r):
-	return rijecnik[r]
+def citanje_zapisa():
+	doc = open(baza, 'r')
+	linije = doc.readlines()
+	for l in linije:
+		print(l, end ='')
+	doc.close()
 
-def razgovor():
-	br = 0
-	for p in range(3):
-		print('Pitanje '+str(p))
-		r = izm[randrange(0, 6)]
-		br += len(r)
-		print(r+'?')
-		print('('+prijevod(r)+')\n')
-		print('Odgovor '+str(p))
-		br += len(prijevod(r))
-		r = izm[randrange(0, 6)]
-		br += len(r)
-		print(r+'?')
-		print('('+prijevod(r)+')\n')
-		br += len(prijevod(r))
-	return br
+def brisanje_zapisa():
+	k = input('Unesi kljuc koji zelite obrisati >> ')
+	doc = open(baza, 'r')
+	linije = doc.readlines()
+	doc.close()
+	doc = open(baza, 'w')
+	for l in linije:
+		if not l.startswith(str(k)):
+			doc.write(l)
+	doc.close()
 
 def main():
-	hrv = ['crveno', 'plavo', 'jedan', 'osam', 'sutra', 'subota']
-	slova = 'abcdefghijklmnopqrstuvwxyz'
-	for hrvrijec in hrv:
-		d = randrange(2, 8)
-		r = slova[randrange(0, len(slova))]
-		for i in range(d-1):
-			s = slova[randrange(0, len(slova))]
-			while s == r[-1]:
-				s = slova[randrange(0, len(slova))]
-			r = r + s
-		rijecnik[r] = hrvrijec
-		izm.append(r)
-
-	print('ukupno '+str(razgovor())+' slova')
+	n = -1
+	while n != 0:
+		print('Odaberite koju naredbu zelite izvrsiti u bazi podataka:')
+		print('1. - unos zapisa')
+		print('2. - citanje zapisa')
+		print('3. brisanje zapisa')
+		print('ZA IZLAZ - 0')
+		n = int(input('>> '))
+		if n == 1:
+			unos_zapisa()
+		if n == 2:
+			citanje_zapisa()
+		if n == 3:
+			brisanje_zapisa()
 
 main()
