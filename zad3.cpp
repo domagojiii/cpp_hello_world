d5e0ca14843752642a86a16fea254dcac6eb01a5
domagojiii <92570830+domagojiii@users.noreply.github.com>
2022-03-31 11:17:08 +0200

    zad3

--- zad3.cpp
+++ zad3.cpp
@@ -1,22 +1,16 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main() {
-  char s1[1000];
-  char s2[1000];
-  cin.getline(s1, 1000);
-  cin.getline(s2, 1000);
-  int rezultat = 0;
-  int i1 = strlen(s1);
-  int i2 = strlen(s2);
-  char c = ' ';
-  for(int i=0; i<i1; i++) {
-    for(int j=0; j<i2; j++) {
-      if(s1[i] == s2[j]) {
-        rezultat++;
-      }
-    }
-  }
-  cout << rezultat << endl;
+  char a;
+
+  printf("Unesi znak:");
+  scanf("%c",&a);
+
+  int x = a & 0b00001000;
+  if(x == 8)
+    printf("Peti bit znaka je %d", 1 );
+  else
+    printf("Peti bit znaka je %d", 0 );
+
   return 0;
 }
